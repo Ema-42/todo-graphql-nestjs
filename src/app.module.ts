f3cd491e7f30a1ee377d4740b95c3f1941ec46e1
emanuel <emanuelchusgp@gmail.com>
2026-01-07 14:45:37 -0400

    primer commit proyecto connestjs y graphql

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { join } from 'path';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { HelloworldModule } from './helloworld/helloworld.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+    HelloworldModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
