c7616ec1640388e7599870b44c8c79e510c30c37
emanuel <emanuelchusgp@gmail.com>
2026-01-08 13:02:12 -0400

    ta

--- src/helloworld/helloworld.resolver.ts
+++ src/helloworld/helloworld.resolver.ts
@@ -1,9 +1,20 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Float, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class HelloworldResolver {
-    @Query(() => String)
-    helloWorld(): string {
-        return 'Hola, Emanuel!';
-    }
+  @Query(() => String, {
+    description: 'A simple hello world query',
+    name: 'hello',
+  })
+  helloWorld(): string {
+    return 'Hola, Emanuel!';
+  }
+
+  @Query(() => Float, {
+    description: 'Devolver numero aleatorios',
+    name: 'random',
+  })
+  randomNumber(): number {
+    return Math.random() * 100;
+  }
 }
